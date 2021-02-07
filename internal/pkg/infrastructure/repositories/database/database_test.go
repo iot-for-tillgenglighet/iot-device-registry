a0f8ae2038661dfd934cae72b4b345f8e1cf10ef
Isak Styf <isak.styf@masarinconsulting.se>
2021-02-07 16:22:43 +0100

    Added a helper method for test database creation

--- internal/pkg/infrastructure/repositories/database/database_test.go
+++ internal/pkg/infrastructure/repositories/database/database_test.go
@@ -16,84 +16,72 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestCreateDatabase(t *testing.T) {
-	_, err := NewDatabaseConnection(NewSQLiteConnector())
-
-	if err != nil {
-		t.Error(err.Error())
-	}
-}
-
 func TestThatCreateDeviceReturnsErrorIfDeviceModelIsNil(t *testing.T) {
-	db, err := NewDatabaseConnection(NewSQLiteConnector())
+	if db, ok := newDatabaseForTest(t); ok {
+		device := fiware.NewDevice("ID", "Value")
 
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	device := fiware.NewDevice("ID", "Value")
-
-	_, err = db.CreateDevice(device)
-	if err == nil || strings.Compare(err.Error(), "CreateDevice requires non-empty device model") != 0 {
-		t.Error(err.Error())
+		_, err := db.CreateDevice(device)
+		if err == nil || strings.Compare(err.Error(), "CreateDevice requires non-empty device model") != 0 {
+			t.Error(err.Error())
+		}
 	}
 }
 
 func TestThatCreateDeviceFailsWithUnknownDeviceModel(t *testing.T) {
-	db, err := NewDatabaseConnection(NewSQLiteConnector())
+	if db, ok := newDatabaseForTest(t); ok {
+		device := fiware.NewDevice("ID", "Value")
+		var err error
 
-	if err != nil {
-		t.Error(err.Error())
-	}
+		device.RefDeviceModel, err = types.NewDeviceModelRelationship("urn:ngsi-ld:DeviceModel:refDeviceModel")
 
-	device := fiware.NewDevice("ID", "Value")
-
-	device.RefDeviceModel, err = types.NewDeviceModelRelationship("urn:ngsi-ld:DeviceModel:refDeviceModel")
-
-	_, err = db.CreateDevice(device)
-	if err == nil || strings.Compare(err.Error(), "No DeviceModel found matching urn:ngsi-ld:DeviceModel:refDeviceModel") != 0 {
-		t.Error(err.Error())
+		_, err = db.CreateDevice(device)
+		if err == nil || strings.Compare(err.Error(), "No DeviceModel found matching urn:ngsi-ld:DeviceModel:refDeviceModel") != 0 {
+			t.Error(err.Error())
+		}
 	}
 }
 
 func TestCreateDeviceModel(t *testing.T) {
-	db, err := NewDatabaseConnection(NewSQLiteConnector())
+	if db, ok := newDatabaseForTest(t); ok {
+		categories := []string{"temperature"}
+		deviceModel := fiware.NewDeviceModel("ID", categories)
 
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	categories := []string{"temperature"}
-	deviceModel := fiware.NewDeviceModel("ID", categories)
-
-	_, err = db.CreateDeviceModel(deviceModel)
-	if err != nil {
-		t.Error("CreateDeviceModel test failed:" + err.Error())
+		_, err := db.CreateDeviceModel(deviceModel)
+		if err != nil {
+			t.Error("CreateDeviceModel test failed:" + err.Error())
+		}
 	}
 }
 
 func TestCreateDevice(t *testing.T) {
+	if db, ok := newDatabaseForTest(t); ok {
+		categories := []string{"T"}
+		deviceModel := fiware.NewDeviceModel("ID2", categories)
+
+		_, err := db.CreateDeviceModel(deviceModel)
+		if err != nil {
+			t.Error("CreateDevice test failed to create device model:" + err.Error())
+		}
+
+		device := fiware.NewDevice("ID", "Value")
+
+		device.RefDeviceModel, err = types.NewDeviceModelRelationship(deviceModel.ID)
+		_, err = db.CreateDevice(device)
+		if err != nil {
+			t.Error("CreateDevice test failed:" + err.Error())
+		}
+	}
+}
+
+func newDatabaseForTest(t *testing.T) (Datastore, bool) {
 	db, err := NewDatabaseConnection(NewSQLiteConnector())
 
 	if err != nil {
 		t.Error(err.Error())
+		return nil, false
 	}
 
-	categories := []string{"T"}
-	deviceModel := fiware.NewDeviceModel("ID2", categories)
-
-	_, err = db.CreateDeviceModel(deviceModel)
-	if err != nil {
-		t.Error("CreateDevice test failed:" + err.Error())
-	}
-
-	device := fiware.NewDevice("ID", "Value")
-
-	device.RefDeviceModel, err = types.NewDeviceModelRelationship(deviceModel.ID)
-	_, err = db.CreateDevice(device)
-	if err != nil {
-		t.Error("CreateDevice test failed:" + err.Error())
-	}
+	return db, true
 }
 
 func deviceFromJSON(deviceJSON string) (*fiware.Device, error) {
